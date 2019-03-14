063501727fcabbe9f9ed85910a4cc2953e8ad1d4
Shane Kelliher <Shane Kelliher@LAPTOP-Q1TB1UGA>
2019-03-14 22:08:39 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+        body {
+        background-image: url("img/wood.jpg")}
+    </style>
+    <title>Shane Kelliher 118397296</title>
+<h1>Shane Kelliher 118397296</h1>
+
+ <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="CV.php">My CV</a>
+  <a href="MyInterests.php">My Interests</a>
+  <a href="AboutConsulting.php">About Consulting</a>
+</div> 
+
+
+
+</html>
